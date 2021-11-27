d0d5d89cbcc0066d1f79a9dd6feec917b469fd0f
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-11-27 18:39:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 function setup() {
     canvas=createCanvas(600,400)
     canvas.position(600,300)
+    video=createCapture(VIDEO)
+    video.size(500,500)
+    poseNet=ml5.poseNet(video,modelloaded)
+    poseNet.on('pose',getposes)
+}
+
+function modelloaded() {
+    console.log("poseNetisloaded")
+}
+
+function getposes(results) {
+    if(results.length>0){
+        console.log(results)
+    }
+}
+
+function draw() {
+    background("cyan")
+
 }
