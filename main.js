29a316d2eae219781d6238483b67318197454667
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-12-11 09:43:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+noseX=0
+noseY=0
+
+leftwristX=0
+rightwristX=0
+
+difference=0
+
 function setup() {
     canvas=createCanvas(600,400)
     canvas.position(600,300)
@@ -14,10 +22,18 @@ function modelloaded() {
 function getposes(results) {
     if(results.length>0){
         console.log(results)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+        leftwristX=results[0].pose.leftWrist.x
+        rightwristx=results[0].pose.rightWrist.x
+        difference=floor(leftwristX-rightwristX)
     }
 }
 
 function draw() {
     background("cyan")
-
+    fill("gold")
+    stroke("firebrick")
+    square(noseX,noseY,difference)
+    document.getElementById("square_side").innerHTML="size of the square:"+difference+" px"
 }
